75c857496981791e8eeb996dbf2bff50a8a82082
Targhib49 <targhib.ibrahim@gmail.com>
2020-04-08 06:01:07 +0700

    insya Allah fixed v10

--- assets/js/register.js
+++ assets/js/register.js
@@ -18,23 +18,36 @@ const userRegistration = (event) => {
 
     //Get data from storage
     let listUser = get();
-    
+
     //User input
     const name = document.getElementById("inputName").value;
     const email = document.getElementById("inputEmail").value;
     const password = document.getElementById("inputPassword").value;
 
-    //Save data to object and storage
-    const userData = {
-        name,
-        email,
-        password,
-    };
-    listUser.push(userData);
-    save(listUser)
+    //Check email availability
+    let sameEmail = false
+    for (let i = 0; i < listUser.length; i++) {
+        if (email == listUser[i].email) {
+            sameEmail = true
+        }
+    }
 
-    //Change to login page
-    window.location.href = `${window.origin}/login.html`;
+    if (!sameEmail) {
+        //Save data to object and storage
+        const userData = {
+            name,
+            email,
+            password,
+        };
+        listUser.push(userData);
+        save(listUser)
+
+        //Change to login page
+        window.location.href = `${window.origin}/login.html`;
+    } else {
+        alert("Your email has already registered")
+    }
+
 }
 
 //Listeners
